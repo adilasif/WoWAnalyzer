66a78f861cf037e56da572300fba50f5ba84f07c
emallson <emallson@emallson.net>
2025-10-25 13:34:38 -0400

    remove miscellaneous unused code [knip]

--- src/common/loadScript.ts
+++
@@ -1,6 +0,0 @@
-export default function loadScript(src: string, type = 'text/javascript') {
-  const script = document.createElement('script');
-  script.type = type;
-  script.src = src;
-  document.body.appendChild(script);
-}
--- src/parser/classic/modules/items/HealthPotionChecker.tsx
+++
@@ -1,5 +0,0 @@
-import InFightConsumablesChecker from 'parser/classic/modules/items/InFightConsumablesChecker';
-
-class HealthPotionChecker extends InFightConsumablesChecker {}
-
-export default HealthPotionChecker;
