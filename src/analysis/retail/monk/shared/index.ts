1726a9db60a0516574ec57232a1378dec977eae6
vohrr <matthew.d.pingel@gmail.com>
2025-11-18 12:53:28 -0500

    fix more typecheck errors

--- src/analysis/retail/monk/shared/index.ts
+++ src/analysis/retail/monk/shared/index.ts
@@ -1,6 +1,5 @@
 export { default as JadefireStomp } from './JadefireStomp';
 export { default as TouchOfDeath } from './TouchOfDeath';
 export { default as MysticTouch } from './MysticTouch';
-export { default as DampenHarm } from './DampenHarm';
 export { default as SaveThemAll } from './SaveThemAll';
 export { default as TeachingsOfTheMonestary } from './TeachingsOfTheMonestary';
