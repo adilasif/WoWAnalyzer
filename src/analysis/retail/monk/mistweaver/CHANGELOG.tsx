59ec97de9cdcaa4d05f52500676bde5561a1c0e1
vohrr <matthew.d.pingel@gmail.com>
2025-11-22 20:06:56 -0500

    changelog

--- src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
+++ src/analysis/retail/monk/mistweaver/CHANGELOG.tsx
@@ -5,7 +5,9 @@ import { ortemis, emallson, swirl, Trevor, Vetyst, Vohrr } from 'CONTRIBUTORS';
 import SpellLink from 'interface/SpellLink';
 
 export default [
-  change(date(2024, 11, 22), <>Update <SpellLink spell={TALENTS_MONK.RUSHING_WIND_KICK_MISTWEAVER_TALENT}/> and <SpellLink spell={TALENTS_MONK.VIVACIOUS_VIVIFICATION_TALENT}/> for Midnight.</>, Vohrr),
+  change(date(2025, 11, 22), <>Updated mastery stats breakdown and removed <SpellLink spell={TALENTS_MONK.REVIVAL_TALENT}/> breakdown.</>, Vohrr),
+  change(date(2025, 11, 22), <>Update <SpellLink spell={TALENTS_MONK.THUNDER_FOCUS_TEA_TALENT}/>, <SpellLink spell={TALENTS_MONK.UPLIFTED_SPIRITS_TALENT}/>, and <SpellLink spell={TALENTS_MONK.JADEFIRE_TEACHINGS_TALENT}/> for Midnight</>, Vohrr),
+  change(date(2025, 11, 22), <>Update <SpellLink spell={TALENTS_MONK.RUSHING_WIND_KICK_MISTWEAVER_TALENT}/> and <SpellLink spell={TALENTS_MONK.VIVACIOUS_VIVIFICATION_TALENT}/> for Midnight.</>, Vohrr),
   change(date(2025, 11, 21), <>Update <SpellLink spell={TALENTS_MONK.MANA_TEA_TALENT}/> for Midnight.</>, Vohrr),
   change(date(2025, 11, 18), <>Initial commit for Midnight.</>, Vohrr),
   change(date(2025, 9, 12), <><SpellLink spell={TALENTS_MONK.COALESCENCE_TALENT}/> now correctly amps <SpellLink spell={SPELLS.ANCIENT_TEACHINGS}/> and Awakened Jadefire healing.</>, swirl),
