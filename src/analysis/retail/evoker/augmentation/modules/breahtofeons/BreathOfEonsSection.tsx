fb0d65e04392954c465197345be59c12dba58227
KYZ124 <46207225+KYZ124@users.noreply.github.com>
2025-12-26 18:02:53 +0000

    Update BreathOfEonsSection.tsx

--- src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsSection.tsx
+++ src/analysis/retail/evoker/augmentation/modules/breahtofeons/BreathOfEonsSection.tsx
@@ -241,28 +241,20 @@ const BreathOfEonsSection: FC<Props> = ({
       <div>
         <p>
           <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> is a powerful cooldown that should be
-          used along side your allies cooldowns, since it's a major damage amplifier.{' '}
-          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> will replicate damage done by your{' '}
-          <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> targets, it is therefore important to
-          maintain 100% uptime on <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> during your{' '}
-          <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> windows.
+          used alongside your allies cooldowns or during increased damage phases, since it's a major
+          damage amplifier, replicating damage done by you and your DPS allies. It also activates
+          many talents, such as <SpellLink spell={TALENTS.DUPLICATE_2_AUGMENTATION_TALENT} />,{' '}
+          <SpellLink spell={TALENTS.OVERLORD_TALENT} />,{' '}
+          <SpellLink spell={TALENTS.IMMINENT_DESTRUCTION_AUGMENTATION_TALENT} />, and{' '}
+          <SpellLink spell={TALENTS.COMMAND_SQUADRON_TALENT} />, greatly increasing your personal
+          damage.
           <br />
         </p>
         <div>
           <p>
-            With <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> talented, you should aim to use{' '}
-            <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> alongside every other{' '}
-            <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} />.{' '}
-            <SpellLink spell={TALENTS.TIME_SKIP_TALENT} /> should be used to reduce the cooldown of
-            your empowers, <SpellLink spell={SPELLS.FIRE_BREATH} /> and{' '}
-            <SpellLink spell={SPELLS.UPHEAVAL} /> to maximize the amount of{' '}
-            <SpellLink spell={SPELLS.SHIFTING_SANDS_BUFF} /> buffs you have active.
-            <br />
-          </p>
-          <p>
             You can use the graph below to visualize your buffs:{' '}
             <SpellLink spell={SPELLS.SHIFTING_SANDS_BUFF} />,{' '}
-            <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> along with your{' '}
+            <SpellLink spell={TALENTS.PRESCIENCE_TALENT} />, and your{' '}
             <SpellLink spell={SPELLS.TEMPORAL_WOUND_DEBUFF} /> debuffs, for each individual{' '}
             <SpellLink spell={TALENTS.BREATH_OF_EONS_TALENT} /> window. Problem points such as:
             letting <SpellLink spell={TALENTS.EBON_MIGHT_TALENT} /> drop during your{' '}
