0d349125006cd3c761eaffbf8d1b0237bf99d166
krealle <vollmer@archon.gg>
2026-01-12 21:04:49 +0100

    changelog

--- src/analysis/retail/evoker/devastation/CHANGELOG.tsx
+++ src/analysis/retail/evoker/devastation/CHANGELOG.tsx
@@ -2,5 +2,6 @@ import { change, date } from 'common/changelog';
 import { Vollmer } from 'CONTRIBUTORS';
 
 export default [
+  change(date(2026, 1, 12), "Update core talent modules and improve Disintegrate analysis accuracy", Vollmer),
   change(date(2026, 1, 9), "Initial Midnight support", Vollmer),
 ];
