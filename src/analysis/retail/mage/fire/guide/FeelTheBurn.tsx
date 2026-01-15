f59aa9ec243419d58832d91c2b9b4dd7a768630a
Sharrq <cjgrasty86@gmail.com>
2026-01-14 21:35:17 -0500

    Guide Rework

--- src/analysis/retail/mage/fire/guide/FeelTheBurn.tsx
+++ src/analysis/retail/mage/fire/guide/FeelTheBurn.tsx
@@ -3,9 +3,7 @@ import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/mage';
 import { SpellLink } from 'interface';
 import Analyzer from 'parser/core/Analyzer';
-import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
-import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
-import { GUIDE_CORE_EXPLANATION_PERCENT } from 'analysis/retail/mage/arcane/Guide';
+import GuideSection from 'interface/guide/components/GuideSection';
 import BuffUptimeBar from 'interface/guide/components/BuffUptimeBar';
 
 import { FEEL_THE_BURN_MAX_STACKS } from '../../shared';
@@ -21,20 +19,6 @@ class FeelTheBurnGuide extends Analyzer {
 
   protected feelTheBurn!: FeelTheBurn;
 
-  get arcaneTempoUptime() {
-    const util = this.feelTheBurn.feelTheBurnUptimeThresholds.actual;
-    const thresholds = this.feelTheBurn.feelTheBurnUptimeThresholds.isLessThan;
-    let performance = QualitativePerformance.Fail;
-    if (util >= thresholds.minor) {
-      performance = QualitativePerformance.Perfect;
-    } else if (util >= thresholds.average) {
-      performance = QualitativePerformance.Good;
-    } else if (util >= thresholds.major) {
-      performance = QualitativePerformance.Ok;
-    }
-    return performance;
-  }
-
   get guideSubsection(): JSX.Element {
     const feelTheBurn = <SpellLink spell={TALENTS.FEEL_THE_BURN_TALENT} />;
     const ignite = <SpellLink spell={SPELLS.IGNITE} />;
@@ -42,32 +26,26 @@ class FeelTheBurnGuide extends Analyzer {
 
     const explanation = (
       <>
-        <div>
-          <b>{feelTheBurn}</b> grants a high amount of mastery which in turn increases your ticking
-          {ignite} damage. Keeping this buff at max stacks is not terribly difficult as it can be
-          extended via {fireBlast}.
-        </div>
+        <b>{feelTheBurn}</b> grants a high amount of mastery which increases your ticking {ignite}{' '}
+        damage. Keeping this buff at max stacks is not difficult as it can be extended via{' '}
+        {fireBlast}.
       </>
     );
+
     const buffHistory = this.selectedCombatant.getBuffHistory(SPELLS.FEEL_THE_BURN_BUFF.id);
 
-    const data = (
-      <BuffUptimeBar
-        spell={TALENTS.FEEL_THE_BURN_TALENT}
-        buffHistory={buffHistory}
-        startTime={this.owner.fight.start_time}
-        endTime={this.owner.fight.end_time}
-        maxStacks={FEEL_THE_BURN_MAX_STACKS}
-        barColor={BURN_COLOR}
-        backgroundBarColor={BURN_BG_COLOR}
-      />
-    );
-
-    return explanationAndDataSubsection(
-      explanation,
-      data,
-      GUIDE_CORE_EXPLANATION_PERCENT,
-      'Feel the Burn',
+    return (
+      <GuideSection spell={TALENTS.FEEL_THE_BURN_TALENT} explanation={explanation}>
+        <BuffUptimeBar
+          spell={TALENTS.FEEL_THE_BURN_TALENT}
+          buffHistory={buffHistory}
+          startTime={this.owner.fight.start_time}
+          endTime={this.owner.fight.end_time}
+          maxStacks={FEEL_THE_BURN_MAX_STACKS}
+          barColor={BURN_COLOR}
+          backgroundBarColor={BURN_BG_COLOR}
+        />
+      </GuideSection>
     );
   }
 }
--- src/analysis/retail/mage/fire/guide/HeatingUp.tsx
+++ src/analysis/retail/mage/fire/guide/HeatingUp.tsx
@@ -1,17 +1,13 @@
 import type { JSX } from 'react';
-import { formatPercentage } from 'common/format';
 import SPELLS from 'common/SPELLS';
 import TALENTS from 'common/TALENTS/mage';
-import { SpellIcon, SpellLink, TooltipElement } from 'interface';
+import { SpellLink } from 'interface';
 import Analyzer from 'parser/core/Analyzer';
-import { RoundedPanel } from 'interface/guide/components/GuideDivs';
-import { explanationAndDataSubsection } from 'interface/guide/components/ExplanationRow';
 import { QualitativePerformance } from 'parser/ui/QualitativePerformance';
-import { PerformanceMark, qualitativePerformanceToColor } from 'interface/guide';
-import { GUIDE_CORE_EXPLANATION_PERCENT } from 'analysis/retail/mage/fire/Guide';
+import CastSummary, { type CastEvaluation } from 'interface/guide/components/CastSummary';
+import GuideSection from 'interface/guide/components/GuideSection';
+
 import HeatingUp from '../core/HeatingUp';
-import CastSummaryAndBreakdown from 'interface/guide/components/CastSummaryAndBreakdown';
-import { BoxRowEntry } from 'interface/guide/components/PerformanceBoxRow';
 
 const CAPPED_MS_THRESHOLD = 7000;
 
@@ -24,96 +20,61 @@ class HeatingUpGuide extends Analyzer {
 
   hasFlameOn: boolean = this.selectedCombatant.hasTalent(TALENTS.FLAME_ON_TALENT);
 
-  generateGuideTooltip(
-    performance: QualitativePerformance,
-    tooltipItems: { perf: QualitativePerformance; detail: string }[],
-    timestamp: number,
-  ) {
-    const tooltip = (
-      <>
-        <div>
-          <b>@ {this.owner.formatTimestamp(timestamp)}</b>
-        </div>
-        <div>
-          <PerformanceMark perf={performance} /> {performance}
-        </div>
-        <div>
-          {tooltipItems.map((t, i) => (
-            <div key={i}>
-              <PerformanceMark perf={t.perf} /> {t.detail}
-              <br />
-            </div>
-          ))}
-        </div>
-      </>
-    );
-    return tooltip;
-  }
+  /**
+   * Evaluates a single Heating Up proc generation for CastSummary.
+   * Returns performance and reason for tooltip display.
+   *
+   * Evaluation priority: good → fail
+   */
+  private evaluateHeatingUpCrit(hu: any): CastEvaluation {
+    const fireBlastCapped =
+      hu.cast.ability.guid === SPELLS.FIRE_BLAST.id &&
+      hu.charges >= (this.hasFlameOn ? 3 : 1) - 1 &&
+      hu.timeTillCapped < CAPPED_MS_THRESHOLD;
 
-  get fireBlastUtil() {
-    const util = this.heatingUp.fireBlastUtilSuggestionThresholds.actual;
-    const thresholds = this.heatingUp.fireBlastUtilSuggestionThresholds.isLessThan;
-    let performance = QualitativePerformance.Fail;
-    if (util > thresholds.minor) {
-      performance = QualitativePerformance.Perfect;
-    } else if (util > thresholds.average) {
-      performance = QualitativePerformance.Good;
-    } else if (util > thresholds.major) {
-      performance = QualitativePerformance.Ok;
+    const castWithoutHeatingUp = !hu.critBuff.active && !hu.hasHotStreak && !hu.hasHeatingUp;
+
+    // GOOD CONDITIONS
+    if (fireBlastCapped) {
+      return {
+        timestamp: hu.cast.timestamp,
+        performance: QualitativePerformance.Good,
+        reason: 'Good - Fire Blast cast while capped on charges',
+      };
     }
-    return performance;
-  }
 
-  get heatingUpData() {
-    const data: BoxRowEntry[] = [];
-    this.heatingUp.heatingUpCrits.forEach((hu) => {
-      const tooltipItems: { perf: QualitativePerformance; detail: string }[] = [];
+    if (hu.critBuff.active && hu.critBuff.buffId) {
+      const buffName = SPELLS[hu.critBuff.buffId]?.name || 'Unknown Buff';
+      return {
+        timestamp: hu.cast.timestamp,
+        performance: QualitativePerformance.Good,
+        reason: `Good - ${hu.cast.ability.name} cast with ${buffName}`,
+      };
+    }
 
-      const fireBlastCapped =
-        hu.cast.ability.guid === SPELLS.FIRE_BLAST.id &&
-        hu.charges >= (this.hasFlameOn ? 3 : 1) - 1 &&
-        hu.timeTillCapped < CAPPED_MS_THRESHOLD;
-      const cappedCharges = fireBlastCapped;
-      if (fireBlastCapped) {
-        tooltipItems.push({
-          perf: QualitativePerformance.Good,
-          detail: `Fire Blast cast while capped`,
-        });
-      }
+    if (!castWithoutHeatingUp) {
+      return {
+        timestamp: hu.cast.timestamp,
+        performance: QualitativePerformance.Good,
+        reason: 'Good - Proper Heating Up generation',
+      };
+    }
 
-      if (!cappedCharges && hu.hasHotStreak) {
-        tooltipItems.push({
-          perf: QualitativePerformance.Fail,
-          detail: `${hu.cast.ability.name} Cast with Hot Streak`,
-        });
-      }
+    // FAIL CONDITIONS
+    if (!fireBlastCapped && castWithoutHeatingUp) {
+      return {
+        timestamp: hu.cast.timestamp,
+        performance: QualitativePerformance.Fail,
+        reason: `${hu.cast.ability.name} cast without Heating Up or crit buff - wasted charge`,
+      };
+    }
 
-      if (!cappedCharges && hu.critBuff.active && hu.critBuff.buffId) {
-        tooltipItems.push({
-          perf: QualitativePerformance.Good,
-          detail: `${hu.cast.ability.name} Cast with ${SPELLS[hu.critBuff.buffId].name}`,
-        });
-      }
-
-      const castWithoutHeatingUp = !hu.critBuff.active && !hu.hasHotStreak && !hu.hasHeatingUp;
-      if (!cappedCharges && castWithoutHeatingUp) {
-        tooltipItems.push({
-          perf: QualitativePerformance.Fail,
-          detail: `${hu.cast.ability.name} Cast without Heating Up or crit buff`,
-        });
-      }
-
-      let overallPerf = QualitativePerformance.Fail;
-      if (cappedCharges || !castWithoutHeatingUp) {
-        overallPerf = QualitativePerformance.Good;
-      }
-
-      if (tooltipItems) {
-        const tooltip = this.generateGuideTooltip(overallPerf, tooltipItems, hu.cast.timestamp);
-        data.push({ value: overallPerf, tooltip });
-      }
-    });
-    return data;
+    // DEFAULT
+    return {
+      timestamp: hu.cast.timestamp,
+      performance: QualitativePerformance.Good,
+      reason: 'Heating Up generated',
+    };
   }
 
   get guideSubsection(): JSX.Element {
@@ -125,66 +86,34 @@ class HeatingUpGuide extends Analyzer {
     const searingTouch = <SpellLink spell={TALENTS.SCORCH_TALENT} />;
     const flamesFury = <SpellLink spell={SPELLS.FLAMES_FURY_BUFF} />;
 
-    const fireBlastIcon = <SpellIcon spell={SPELLS.FIRE_BLAST} />;
-
     const explanation = (
       <>
-        <div>
-          While <b>{heatingUp}</b> is not as impactful as {hotStreak}, it is important to properly
-          manage your {heatingUp} procs to get as many {hotStreak} procs as possible throughout the
-          fight.
-        </div>
-        <div>
-          <ul>
-            <li>
-              Use guaranteed crit abilities like {fireBlast} (with ) to convert {heatingUp} to{' '}
-              {hotStreak}
-            </li>
-            <li>
-              Unless you are guaranteed to crit ({combustion}, {firestarter}, {searingTouch}), , or
-              are capped or about to cap on charges, don't use {fireBlast}
-              without {heatingUp}.
-            </li>
-            <li>
-              Outside of {combustion} you can use without {heatingUp}, and then convert that into{' '}
-              {hotStreak} with {fireBlast}, especially if you have {flamesFury} procs.
-            </li>
-          </ul>
-        </div>
+        <b>{heatingUp}</b> is essential for managing your {hotStreak} procs. Properly managing{' '}
+        {heatingUp} maximizes your {hotStreak} generation throughout the fight.
+        <ul>
+          <li>
+            Use guaranteed crit abilities like {fireBlast} to convert {heatingUp} to {hotStreak}.
+          </li>
+          <li>
+            Unless you are guaranteed to crit ({combustion}, {firestarter}, {searingTouch}), or are
+            capped/about to cap on charges, don't use {fireBlast} without {heatingUp}.
+          </li>
+          <li>
+            Outside of {combustion}, you can use {fireBlast} without {heatingUp} and then convert
+            that into {hotStreak}, especially with {flamesFury} procs.
+          </li>
+        </ul>
       </>
-    );
-    const fireBlastTooltip = (
-      <>
-        {this.heatingUp.fireBlastWithoutHeatingUp} Fire Blast casts without Heating Up.
-        <br />
-        {this.heatingUp.fireBlastsDuringHotStreak} Fire Blasts cast during Hot Streak
-      </>
-    );
-    const data = (
-      <div>
-        <RoundedPanel>
-          <div
-            style={{ color: qualitativePerformanceToColor(this.fireBlastUtil), fontSize: '20px' }}
-          >
-            {fireBlastIcon}{' '}
-            <TooltipElement content={fireBlastTooltip}>
-              {formatPercentage(this.heatingUp.fireBlastUtilPercent, 0)} %{' '}
-              <small>Fire Blast Utilization</small>
-            </TooltipElement>
-          </div>
-          <div>
-            <br />
-            <CastSummaryAndBreakdown spell={SPELLS.HEATING_UP} castEntries={this.heatingUpData} />
-          </div>
-        </RoundedPanel>
-      </div>
     );
 
-    return explanationAndDataSubsection(
-      explanation,
-      data,
-      GUIDE_CORE_EXPLANATION_PERCENT,
-      'Heating Up',
+    return (
+      <GuideSection spell={SPELLS.HEATING_UP} explanation={explanation}>
+        <CastSummary
+          spell={SPELLS.HEATING_UP}
+          casts={this.heatingUp.heatingUpCrits.map((crit) => this.evaluateHeatingUpCrit(crit))}
+          showBreakdown
+        />
+      </GuideSection>
     );
   }
 }
