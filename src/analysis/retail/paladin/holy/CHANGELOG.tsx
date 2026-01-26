9435cc85688115dc99e44452a9cdae176c7fd27e
swirl <180882683+lolswirl@users.noreply.github.com>
2026-01-26 14:54:47 -0500

    update crusaders might

--- src/analysis/retail/paladin/holy/CHANGELOG.tsx
+++ src/analysis/retail/paladin/holy/CHANGELOG.tsx
@@ -1,6 +1,9 @@
 import { change, date } from 'common/changelog';
+import { TALENTS_PALADIN } from 'common/TALENTS/paladin';
 import { swirl } from 'CONTRIBUTORS';
+import SpellLink from 'interface/SpellLink';
 
 export default [
+  change(date(2026, 1, 27), <>Updated <SpellLink spell={TALENTS_PALADIN.CRUSADERS_MIGHT_TALENT} /> module.</>, swirl),
   change(date(2026, 1, 3), <>Initial Holy Paladin support for Midnight.</>, swirl),
 ];
